aa3c5f1fb3070e683727013fa1c64a1084ab0f18
Arthur Santos Silva <arthurssilva07@gmail.com>
2023-05-03 11:47:57 -0400

    Update arthur_morris-create(x).R

--- arthur_morris-create(x).R
+++ arthur_morris-create(x).R
@@ -1,38 +1,39 @@
-###--Esta FUNÇÃO tem o objetivo de determinar uma amostra com o método de Morris.
+###--This function has the goal to create a sample with the Morris method, in order to be analyzed further.
 
-pasta=c("Tese_Parte 2/2. Morris SA")
+#---input arguments for the function
+pasta=c("name of the folder/subfolder")
 input.r=80
 input.l=8
 
 #morris.create(pasta,input.r,input.l)
 morris.create=function (pasta,input.r=10,input.l=4){
-      #---Abrir o package 'sensitivity'
+      #---Open the 'sensitivity' package
       library(sensitivity)
       
-      #---Arquivo que contém os dados dos fatores "input.csv"
+      #---File which contains the factors data "input.csv"
       factors.path=c(paste(pasta,"/morris-input.csv",sep=""))
-      #---Class de cada coluna do arquivo de entrada "input.csv"
+      #---Class from each column from the input file "input.csv"
       factors.classes=c("character","character","numeric","numeric")
-      #---Ler o arquivo de entrada "input.csv"
+      #---Read the input file "input.csv"
       factors=read.table(file=factors.path,header=TRUE,sep=",",colClasses=factors.classes)
       
-      #---Discretizar o arquivo para facilitar os cálculos
+      #---Discretize the file to facilitate the calculation
       factors.names=factors[[1]]
       factors.lower=factors[[3]]
       factors.upper=factors[[4]]
       
-      #---Aplicar a função "morris()" para calcular a amostra e todas as demais informações
+      #---Apply the morris() function to create a sample and other information
       morris.funcao=morris(model=NULL,factors=factors.names,r=input.r,
                            design=list(type="oat",levels=input.l,grid.jump=1),
                            binf=factors.lower,bsup=factors.upper,scale=FALSE)
       
-      #---Salvar o arquivo gerado em formato R para ler depois de simular, sem perder a amostra
+      #---Save the file in R format without losing the sample information
       morris.dput=dput(morris.funcao,file=c(paste(pasta,"/morris-dput.R",sep="")),control="all")
       
-      #---Salvar a amostra em CSV
+      #---Save the sample in CSV format, also
       morris.amostra=morris.funcao[[8]]
       morris.amostra.path=c(paste(pasta,"/morris-sample.csv",sep=""))
       morris.amostra.csv=write.table(x=morris.amostra,file=morris.amostra.path,sep=",",row.names=FALSE)
       
-      return(c("Arquivos de Morris criados com sucesso *-*"))
+      return(c("Morris sample created with success *-*"))
 }
